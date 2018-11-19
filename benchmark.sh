13e674b3baa464df3a3beb92e1666e24ab83f055
Leo Jiménez <leojimenezcr@gmail.com>
2018-11-18 19:22:56 -0600

    Implementación de insert y delete en Oracle

--- benchmark.sh
+++ benchmark.sh
@@ -55,8 +55,19 @@ function oracle_stop() {
 function oracle_insert(){
   echo "oracle_insert" >> $LOGFILE
   
-  # Example: docker-compose exec oracle-12c sh -c 'echo @/u01/app/oracle/script.sql | sqlplus -s system/oracle'
+  # SQL script generation
+  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "INSERT INTO ontime VALUES(\x27" $1 "\x27, \x27" $2 "\x27, \x27" $3 "\x27, \x27" $4 "\x27, \x27" $5 "\x27, \x27" $6 "\x27, \x27" $7 "\x27, \x27" $8 "\x27, \x27" $9 "\x27, \x27" $10 "\x27, \x27" $11 "\x27, \x27" $12 "\x27, \x27" $13 "\x27, \x27" $14 "\x27, \x27" $15 "\x27, \x27" $16 "\x27, \x27" $17 "\x27, \x27" $18 "\x27, \x27" $19 "\x27, \x27" $20 "\x27, \x27" $21 "\x27, \x27" $22 "\x27, \x27" $23 "\x27, \x27" $24 "\x27, \x27" $25 "\x27, \x27" $26 "\x27, \x27" $27 "\x27, \x27" $28 "\x27, \x27" $29 "\x27);" }' > $ORACLEPATH/oracle-data/testscript.sql
+
+  #docker-compose exec oracle-12c sh -c 'echo @/u01/app/oracle/testscript.sql | sqlplus -s system/oracle'
 }
+
+function oracle_clear(){
+  echo "oracle_clear" >> $LOGFILE
+  
+  docker-compose exec oracle-12c sh -c 'echo "DELETE FROM ontime;" | sqlplus -s system/oracle'
+}
+
+
 function oracle_sort() {
   echo "oracle_sort" >> $LOGFILE
 }
@@ -87,25 +98,25 @@ function redis_sort() {
 for DATASIZE in ${DATASIZELIST[@]}
 do  
   echo "Starting test with $DATASIZE records." >> $LOGFILE
-  # Random number from 1 to (Source file number of lines - Test data size)
+  # Random number from 1 to (Source file number of lines - Test data size + 1)
   RANDOMLINE=$( shuf -n1 -i1-$(( $(wc -l < $SOURCEFILE) - $DATASIZE + 1 )) )
-  echo $RANDOMLINE
 
   # Oracle test
-#  oracle_start
+  oracle_start
   oracle_insert
   take_start_time
     oracle_sort
   take_end_time
-#  oracle_stop
+  oracle_clear
+  oracle_stop
 
   # Redis test
-#  redis_start  
+  redis_start  
   redis_insert  
   take_start_time
     oracle_sort
   take_end_time  
-#  redis_stop
+  redis_stop
   
   echo "Ending test for $DATASIZE records." >> $LOGFILE
   echo "" >> $LOGFILE
