5d8ee204276f81ac093479cd3bbfb5fa1ea61fd6
Curso CI <geovanny.corderovalverde@ucr.ac.cr>
2018-11-28 18:34:50 -0600

    Ingresa datos en el hasmap correctamente

--- benchmark.sh
+++ benchmark.sh
@@ -6,9 +6,9 @@
 
 ## SETUP ##
 SOURCEFILE=$(pwd)/testrecords.csv
-DATASIZELIST=(1000 10000)
+DATASIZELIST=(100)
 #DATASIZELIST=(1000 10000 100000 1000000)
-TESTREPETITIONS=2
+TESTREPETITIONS=1
 LOGFILE=$(pwd)/benchmark.log
 CSVFILE=$(pwd)/$(date +%Y%m%d-%H%M)-benchmark.csv
 
@@ -99,7 +99,7 @@ function redis_insert() {
   echo "Inserting records" >> $LOGFILE
 
   # SQL script generation
-  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "HSET " $1$2$3$4$5 " Year " $1 " Month " $2 " DayofMonth " $3 " DayofWeek " $4 " DepTime " $5 " CRSDepTime " $6 " ArrTime " $7 " CRSArrTime " $8 " UniqueCarrier " $9 " FlightNum " $10 " TailNum " $11 " ActualElapsedTime " $12 " CRSElapsedTime " $13 " AirTime " $14 " ArrDelay " $15 " DepDelay " $16 " Origin " $17 " Dest " $18 " Distance " $19 " TaxiIn " $20 " TaxiOut " $21 " Cancelled " $22 "\n"}' > $REDISPATH/redis-data/testscript.txt
+  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "HSET " $1$2$3$4$5 " Year " $1 " Month " $2 " DayofMonth " $3 " DayofWeek " $4 " DepTime " $5 " CRSDepTime " $6 " ArrTime " $7 " CRSArrTime " $8 " UniqueCarrier " $9 " FlightNum " $10 " TailNum " $11 " ActualElapsedTime " $12 " CRSElapsedTime " $13 " AirTime " $14 " ArrDelay " $15 " DepDelay " $16 " Origin " $17 " Dest " $18 " Distance " $19 " TaxiIn " $20 " TaxiOut " $21 " Cancelled " $22 "\\n"}' > $REDISPATH/redis-data/testscript.txt
   ## hace falta añadir literalmente un \n al final del string y añadir el lpush 
   docker-compose exec redis sh -c 'echo $(cat testscript.txt) | redis-cli -c'
 }
@@ -115,32 +115,32 @@ echo "Results: $CSVFILE"
 
 for DATASIZE in ${DATASIZELIST[@]}
 do
-  for (( TESTITERACTION=1; TESTITERACTION<=$TESTREPETITIONS; TESTITERACTION++ ))
-  do
+  #for (( TESTITERACTION=1; TESTITERACTION<=$TESTREPETITIONS; TESTITERACTION++ ))
+  #do
     echo "Starting test $TESTITERACTION of $TESTREPETITIONS with $DATASIZE records." >> $LOGFILE
     # Random number from 1 to (Source file number of lines - Test data size + 1)
     RANDOMLINE=$( shuf -n1 -i1-$(( $(wc -l < $SOURCEFILE) - $DATASIZE + 1 )) )
 
     # Oracle test
-    oracle_start
-    oracle_insert
-    take_start_time
-      oracle_sort
-    take_end_time
-    oracle_clear
-    oracle_stop
+    #oracle_start
+    #oracle_insert
+    #take_start_time
+    #  oracle_sort
+    #take_end_time
+    #oracle_clear
+    #oracle_stop
 
     # Redis test
-    #redis_start  
-    #redis_insert  
-    #take_start_time
-    #  redis_sort
-    #take_end_time  
-    #redis_stop
+    redis_start  
+    redis_insert  
+    take_start_time
+      redis_sort
+    take_end_time  
+    redis_stop
     
-    echo "Finished test $TESTITERACTION of $TESTREPETITIONS for $DATASIZE records." >> $LOGFILE
+    #echo "Finished test $TESTITERACTION of $TESTREPETITIONS for $DATASIZE records." >> $LOGFILE
     echo "" >> $LOGFILE
-  done #test repetitions
+  #done #test repetitions
 done #data size list
 
 echo "Finished all tests." >> $LOGFILE
