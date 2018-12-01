24c12e0f21f502b5380f1fac657ca40f94665b71
Leo Jiménez <leojimenezcr@gmail.com>
2018-12-01 17:38:17 -0600

    Toma de tiempo de inserts

--- benchmark.sh
+++ benchmark.sh
@@ -61,17 +61,18 @@ function oracle_stop() {
   cd ..
 }
 
+function oracle_insert_script_generation(){
+  echo "Generating Oracle script" >> $LOGFILE
+  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "INSERT INTO ontime VALUES(\x27" $1$2$3$4$5 "\x27, \x27" $1 "\x27, \x27" $2 "\x27, \x27" $3 "\x27, \x27" $4 "\x27, \x27" $5 "\x27, \x27" $6 "\x27, \x27" $7 "\x27, \x27" $8 "\x27, \x27" $9 "\x27, \x27" $10 "\x27, \x27" $11 "\x27, \x27" $12 "\x27, \x27" $13 "\x27, \x27" $14 "\x27, \x27" $15 "\x27, \x27" $16 "\x27, \x27" $17 "\x27, \x27" $18 "\x27, \x27" $19 "\x27, \x27" $20 "\x27, \x27" $21 "\x27, \x27" $22 "\x27);" }' > $ORACLEPATH/oracle-data/testscript.sql
+}
+
 function oracle_insert(){
   echo "Inserting records" >> $LOGFILE
-  
-  # SQL script generation
-  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "INSERT INTO ontime VALUES(\x27" $1$2$3$4$5 "\x27, \x27" $1 "\x27, \x27" $2 "\x27, \x27" $3 "\x27, \x27" $4 "\x27, \x27" $5 "\x27, \x27" $6 "\x27, \x27" $7 "\x27, \x27" $8 "\x27, \x27" $9 "\x27, \x27" $10 "\x27, \x27" $11 "\x27, \x27" $12 "\x27, \x27" $13 "\x27, \x27" $14 "\x27, \x27" $15 "\x27, \x27" $16 "\x27, \x27" $17 "\x27, \x27" $18 "\x27, \x27" $19 "\x27, \x27" $20 "\x27, \x27" $21 "\x27, \x27" $22 "\x27);" }' > $ORACLEPATH/oracle-data/testscript.sql
-
   docker-compose exec oracle-12c sh -c 'echo @/u01/app/oracle/testscript.sql | sqlplus -s system/oracle' &> /dev/null
 }
 
 function oracle_clear(){
-  echo "Cleaning oracle docker" >> $LOGFILE
+  echo "Cleaning Oracle docker" >> $LOGFILE
   
   docker-compose exec oracle-12c sh -c 'echo "DELETE FROM ontime;" | sqlplus -s system/oracle' &> /dev/null
 }
@@ -100,18 +101,18 @@ function redis_stop() {
   cd ..
 }
 
+function redis_insert_script_generation(){
+  echo "Generating redis script" >> $LOGFILE
+  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "HSET " $1$2$3$4$5 " Year " $1 " Month " $2 " DayofMonth " $3 " DayofWeek " $4 " DepTime " $5 " CRSDepTime " $6 " ArrTime " $7 " CRSArrTime " $8 " UniqueCarrier " $9 " FlightNum " $10 " TailNum " $11 " ActualElapsedTime " $12 " CRSElapsedTime " $13 " AirTime " $14 " ArrDelay " $15 " DepDelay " $16 " Origin " $17 " Dest " $18 " Distance " $19 " TaxiIn " $20 " TaxiOut " $21 " Cancelled " $22 " \\n \nLPUSH ontime " $1$2$3$4$5 " \\n"}' > $REDISPATH/redis-data/testscript.txt
+}
+
 function redis_insert() {
   echo "Inserting records" >> $LOGFILE
-
-  # SQL script generation
-  tail --lines=+$RANDOMLINE $SOURCEFILE | head -n $DATASIZE | awk -F',' '{ print "HSET " $1$2$3$4$5 " Year " $1 " Month " $2 " DayofMonth " $3 " DayofWeek " $4 " DepTime " $5 " CRSDepTime " $6 " ArrTime " $7 " CRSArrTime " $8 " UniqueCarrier " $9 " FlightNum " $10 " TailNum " $11 " ActualElapsedTime " $12 " CRSElapsedTime " $13 " AirTime " $14 " ArrDelay " $15 " DepDelay " $16 " Origin " $17 " Dest " $18 " Distance " $19 " TaxiIn " $20 " TaxiOut " $21 " Cancelled " $22 " \\n \nLPUSH ontime " $1$2$3$4$5 " \\n"}' > $REDISPATH/redis-data/testscript.txt
-  ## hace falta añadir literalmente un \n al final del string y añadir el lpush 
   docker-compose exec redis sh -c 'echo $(cat testscript.txt) | redis-cli -c' &> /dev/null
 }
 
 function redis_sort() {
   echo "Sorting records" >> $LOGFILE
-
   docker-compose exec redis redis-cli SORT ontime &> /dev/null
 }
 
@@ -130,19 +131,28 @@ do
 
     # Oracle test
     oracle_start
-    oracle_insert
+    oracle_insert_script_generation
+    
+    take_start_time
+      oracle_insert
+    take_end_time
+    
     take_start_time
       oracle_sort
     take_end_time
+    
     oracle_clear
     oracle_stop
 
     # Redis test
-    redis_start  
-    redis_insert  
+    redis_start
+    take_start_time
+      redis_insert
+    take_end_time
+
     take_start_time
       redis_sort
-    take_end_time  
+    take_end_time
     redis_stop
     
     echo "Finished test $TESTITERACTION of $TESTREPETITIONS for $DATASIZE records." >> $LOGFILE
